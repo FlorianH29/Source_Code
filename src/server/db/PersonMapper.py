e7c49fcb00791354c109f65bd06ccea88bbd253d
benedikt <benelere@gmail.com>
2022-05-12 16:47:00 +0200

    PersonMapper Fehler korrigiert

--- src/server/db/PersonMapper.py
+++ src/server/db/PersonMapper.py
@@ -122,11 +122,14 @@ class PersonMapper(Mapper):
 
         :param employee das Objekt, das in die DB geschrieben werden soll
         """
+
+
         cursor = self._cnx.cursor()
 
-        command = "UPDATE person " + "SET firstName=%s, last_edit=%s, lastName=%s, username=%s, mailaddress=%s," \
+        command = "UPDATE person " + "SET firstName='Bene', last_edit=%s, lastName=%s, username=%s, mailaddress=%s," \
                                      "firebase_id=%s WHERE person_id=%s"
-        data = (employee.get_first_name(), employee.get_last_edit(), employee.get_username, employee.get_mailadress,
+
+        data = (employee.get_firstname(), employee.get_last_edit(), employee.get_username, employee.get_mailaddress,
                 employee.get_id(), employee.get_firebase_id)
         cursor.execute(command, data)
 
