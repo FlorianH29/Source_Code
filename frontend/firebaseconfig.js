5b701adcac36d30272db6897cf2454bdc009e821
FlorianH29 <fs155@hdm-stuttgart.de>
2022-05-18 16:48:48 +0200

    Save bei TimeIntrval in App überarbeitet Dump angepasst auf time_period

---
+++ frontend/firebaseconfig.js
@@ -0,0 +1,17 @@
+////npm install firebase
+/// Package: google-auth
+
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMMX3fFV7pouknyu-bOHahxB9khhB_QPw",
+  authDomain: "sopra-17-fb.firebaseapp.com",
+  projectId: "sopra-17-fb",
+  storageBucket: "sopra-17-fb.appspot.com",
+  messagingSenderId: "1057557907859",
+  appId: "1:1057557907859:web:c42687f5cedeec80864066"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
