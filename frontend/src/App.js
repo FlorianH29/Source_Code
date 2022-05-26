994053284684c8e9218d2ced52aadad87b58d48d
milenaKuebler <milli1999@gmx.de>
2022-05-26 19:48:11 +0200

    Ausgabe aller Personen im Frontend. Anpassung der App.js / HdMWepAppApi,js / main.py / PersonBO,js/ PersonList.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import {HdMWebAppAPI} from "./api";
 import {PersonBO} from "./api";
+import PersonList from './components/PersonList';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        // Init an empty state
+        this.state = {
+            currentUser: null
+        };
+    }
+
+    render() {
+        return (
+            <Router basename={process.env.PUBLIC_URL}>
+
+                {
+                    true ?
+                        <>
+                            <Redirect from='/' to='persons'/>
+                            <Route exact path='/persons'>
+                                <PersonList/>
+                            </Route>
+                        </>
+                        :
+                        <>
+                            <p>Sign In </p>
+
+                        </>
+                }
+            </Router>
+        );
+    }
 }
 
 export default App;
