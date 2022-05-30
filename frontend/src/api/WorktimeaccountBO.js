639c799b16c48f5aeb1d72cbf33b62a68b55eb32
marie <marie.roth22@gmail.com>
2022-05-30 20:15:52 +0200

    Aktualisierung WorktimeAccountBO.js

--- frontend/src/api/WorktimeaccountBO.js
+++ frontend/src/api/WorktimeaccountBO.js
@@ -1,54 +1,27 @@
 import BusinessObject from './BusinessObject';
 
-/**
- * Represents an account object of a person.
- */
-export default class WorktimeaccountBO extends BusinessObject {
+export default class WorktimeAccountBO extends BusinessObject {
 
-  /**
-   * Constructs a new AccountBO object with a given owner.
-   *
-   * @param {*} aOwner - the owner of this AccountBO.
-   */
-  constructor(aOwner) {
-    super();
-    this.owner = aOwner;
-  }
-
-  /**
-   * Sets the owner of this AccountBO.
-   *
-   * @param {*} aOwner - the new owner of this AccountBO.
-   */
-  setOwner(aOwner) {
-    this.owner = aOwner;
-  }
-
-  /**
-   * Gets the owner of this AccountBO.
-   */
-  getOwner() {
-    return this.owner;
-  }
-
-  /**
-   * Returns an Array of AccountBOs from a given JSON structure
-   */
-  static fromJSON(worktimeaccounts) {
-    let result = [];
-
-    if (Array.isArray(worktimeaccounts)) {
-      worktimeaccounts.forEach((a) => {
-        Object.setPrototypeOf(a, WorktimeaccountBO.prototype);
-        result.push(a);
-      })
-    } else {
-      // Es handelt sich offenbar um ein singuläres Objekt
-      let a = worktimeaccounts;
-      Object.setPrototypeOf(a, WorktimeaccountBO.prototype);
-      result.push(a);
+    constructor() {
+        super();
     }
 
-    return result;
-  }
+    static fromJSON(worktimeaccount) {
+        let result = [];
+
+        if (Array.isArray(worktimeaccount)) {
+            worktimeaccount.forEach((w) => {
+                Object.setPrototypeOf(w, WorktimeAccountBO.prototype);
+                result.push(w);
+            })
+        } else {
+            // Es handelt sich offenbar um ein singuläres Objekt
+            let w = worktimeaccount;
+            Object.setPrototypeOf(w, WorktimeAccountBO.prototype);
+            result.push(w);
+        }
+
+        //console.log(result)
+        return result;
+    }
 }
