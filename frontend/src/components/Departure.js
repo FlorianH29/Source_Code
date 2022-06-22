4d95f533bcbea5bf8a40fcc2d8b8ee0c42c66662
JannikLueers <jannik.lueers@t-online.de>
2022-06-22 20:36:12 +0200

    DepartureDialog eingefügt. Der Mitarbeiter wird nun beim Drücken des "Gehen"-Buttons in einem Dialog gefragt, ob er wirklich gehen und sich abmelden möchte. Drückt der Mitarbeiter dann auf den "Gehen & Abmelden"-Button erzeugt dieser ein Gehen-Event und wird abgemeldet.

--- frontend/src/components/Departure.js
+++ frontend/src/components/Departure.js
@@ -8,6 +8,8 @@ import {DialogContentText, DialogTitle, IconButton} from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 import DepartureBO from "../api/DepartureBO";
 import firebase from "firebase/compat/app";
+import PersonDeleteDialog from "./dialogs/PersonDeleteDialog";
+import DepartureDialog from "./dialogs/DepartureDialog";
 
 class Departure extends Component {
 
@@ -16,18 +18,16 @@ class Departure extends Component {
 
         this.state = {
             openDepartureDialog: false,  // Status der Anzeige von Dialog, ob der Mitarbeiter wirklich gehen möchte
-            disableDeparture: false,  // Gehen-Button wird disabled, wenn der Mitarbeiter in einer Aktivität oder Pause ist.
             updatingError: null
         }
     }
 
-    /** Erstellen eines Departure-Events */
+    /** Erstellen eines Departure-Events und Abmelden des Mitarbeiters*/
     addNewDepartureEvent = () => {
         // Umschalten des Status der Knöpfe
         this.setState({
 
             openDepartureDialog: true,
-            disableDeparture: false,
             updatingError: null
         });
         // Erstellen eines Gehen-Ereignis
@@ -38,13 +38,27 @@ class Departure extends Component {
             this.setState(this.baseState);
             this.props.onClose(departure); // call the parent with the departure object from backend
         }).catch(e =>
-            this.setState({
-                updatingInProgress: false,    // disable loading indicator
-                updatingError: e              // show error message
-            })
+            console.log(e)
         );
         // Mitarbeiter ausloggen, wenn Gehen-Event erstellt wurde
         firebase.auth().signOut();
+    }
+
+    handleDepartureButtonClicked = () => {
+        // Umschalten des Status der Knöpfe
+        this.setState({
+
+            openDepartureDialog: true
+        });
+
+    }
+
+
+    /** Schließen des DepartureDialogs */
+    handleCloseDepartureDialog = () => {
+      this.setState({
+          openDepartureDialog: false
+      })
     }
 
     // let newCustomer = new CustomerBO(this.state.firstName, this.state.lastName);
@@ -63,9 +77,7 @@ class Departure extends Component {
 
     /** Rendern der Komponente */
     render() {
-        const {classes} = this.props;
         const {openDepartureDialog} = this.state;
-        // console.log(this.state)
 
         return (
             <div>
@@ -74,7 +86,7 @@ class Departure extends Component {
                         <Grid container justifyContent={"center"}>
                             <Grid item xs={6} align={"center"}>
                                 <Button variant='contained' color='primary'
-                                        onClick={this.addNewDepartureEvent}>
+                                        onClick={this.handleDepartureButtonClicked}>
                                     Gehen
                                 </Button>
                             </Grid>
@@ -82,9 +94,22 @@ class Departure extends Component {
                     </ListItem>
                     <Divider/>
                 </Box>
+                <DepartureDialog show={openDepartureDialog} onClose={this.handleCloseDepartureDialog}></DepartureDialog>
             </div>
         );
     }
+}
+
+Departure.propTypes = {
+  /** Das ProjectWorkBO, das gelöscht werden soll */
+  person: PropTypes.object.isRequired,
+  /** Wenn show true ist, wird der Dialog gerendert */
+  show: PropTypes.bool.isRequired,
+  /**
+   * Handler Funktion, die aufgerufen wird, wenn der Dialog geschlossen wird.
+   * Sendet das gelöschte ProjectWorkBO as Parameter oder null, wenn Abbrechen aufgerufen worden ist.
+   */
+  onClose: PropTypes.func.isRequired,
 }
 
 export default Departure;
